582f6dc213cce4da42ac2f0e65ccb3507cebf9f3
chushaa <31249563+chushaa@users.noreply.github.com>
2017-09-25 08:54:25 -0500

    Add files via upload
    Initial COmmit

---
+++ nbastats.js
@@ -0,0 +1,176 @@
+var data = [
+  {
+    "team": "Golden State Warriors",
+    "win": 67,
+    "loss": 15,
+    "ppg": 115.9
+  },
+  {
+    "team": "San Antonio Spurs",
+    "win": 61,
+    "loss": 21,
+    "ppg": 105.3
+  },
+  {
+    "team": "Houston Rockets",
+    "win": 55,
+    "loss": 27,
+    "ppg": 115.3
+  },
+  {
+    "team": "Boston Celtics",
+    "win": 53,
+    "loss": 29,
+    "ppg": 108
+  },
+  {
+    "team": "Utah Jazz",
+    "win": 51,
+    "loss": 31,
+    "ppg": 100.7
+  },
+  {
+    "team": "Tornoto Raptors",
+    "win": 51,
+    "loss": 31,
+    "ppg": 106.9
+  },
+  {
+    "team": "Cleveland Cavaliers",
+    "win": 51,
+    "loss": 31,
+    "ppg": 110.3
+  },
+  {
+    "team": "Los Angeles Clippers",
+    "win": 51,
+    "loss": 31,
+    "ppg": 108.7
+  },
+  {
+    "team": "Washington Wizards",
+    "win": 49,
+    "loss": 33,
+    "ppg": 109.2
+  },
+  {
+    "team": "Oklahoma City Thunder",
+    "win": 47,
+    "loss": 35,
+    "ppg": 106.6
+  },
+  {
+    "team": "Memphis Grizzlies",
+    "win": 43,
+    "loss": 39,
+    "ppg": 100.5
+  },
+  {
+    "team": "Atlanta Hawks",
+    "win": 43,
+    "loss": 39,
+    "ppg": 103.2
+  },
+  {
+    "team": "Indiana Pacers",
+    "win": 42,
+    "loss": 40,
+    "ppg": 105.1
+  },
+  {
+    "team": "Milwaukee Bucks",
+    "win": 42,
+    "loss": 40,
+    "ppg": 103.6
+  },
+  {
+    "team": "Chicago Bulls",
+    "win": 41,
+    "loss": 41,
+    "ppg": 102.9
+  },
+  {
+    "team": "Portland Trail Blazers",
+    "win": 41,
+    "loss": 41,
+    "ppg": 107.9
+  },
+  {
+    "team": "Miami Heat",
+    "win": 41,
+    "loss": 41,
+    "ppg": 103.2
+  },
+  {
+    "team": "Dever Nuggets",
+    "win": 40,
+    "loss": 42,
+    "ppg": 111.7
+  },
+  {
+    "team": "Detroit Pistons",
+    "win": 37,
+    "loss": 45,
+    "ppg": 101.3
+  },
+  {
+    "team": "Charlotte Hornets",
+    "win": 36,
+    "loss": 46,
+    "ppg": 104.9
+  },
+  {
+    "team": "New Orleans Pelicans",
+    "win": 34,
+    "loss": 48,
+    "ppg": 104.3
+  },
+  {
+    "team": "Dallas Mavericks",
+    "win": 33,
+    "loss": 49,
+    "ppg": 97.9
+  },
+  {
+    "team": "Sacramento Kings",
+    "win": 32,
+    "loss": 50,
+    "ppg": 102.8
+  },
+  {
+    "team": "Minnesota Timberwolves",
+    "win": 31,
+    "loss": 51,
+    "ppg": 105.6
+  },
+  {
+    "team": "Orlando Magic",
+    "win": 29,
+    "loss": 53,
+    "ppg": 101.1
+  },
+  {
+    "team": "Philadelphia 76ers",
+    "win": 28,
+    "loss": 54,
+    "ppg": 102.4
+  },
+  {
+    "team": "Los Angeles Lakers",
+    "win": 26,
+    "loss": 56,
+    "ppg": 104.6
+  },
+  {
+    "team": "Phoenix Suns",
+    "win": 24,
+    "loss": 58,
+    "ppg": 107.7
+  },
+  {
+    "team": "Brooklyn Nets",
+    "win": 20,
+    "loss": 62,
+    "ppg": 105.8
+  }
+];
